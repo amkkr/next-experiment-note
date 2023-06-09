661a4202f6c7f0aeb1dbfc799011b6d20064cc9f
amkkr <55781271+amkkr@users.noreply.github.com>
2023-06-09 22:34:55 +0900

    fix: add mocking global object

--- jest.config.ts
+++ jest.config.ts
@@ -9,14 +9,13 @@ const config: Config = {
   preset: "ts-jest",
   roots: ["<rootDir>/src"],
   moduleDirectories: ["node_modules", "src"],
-  transformIgnorePatterns: [
-    `/node_modules/(?!${esModules})`
-  ],
+  transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
   testEnvironment: "jest-environment-jsdom",
   moduleNameMapper: {
-    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.ts",
-  }
-}
+    "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.ts",
+  },
+};
 
 const asyncConfig = createJestConfig(config);
 export default async () => {
--- src/components/image/__tests__/ExampleDiv.test.tsx
+++ src/components/image/__tests__/ExampleDiv.test.tsx
@@ -1,33 +1,56 @@
-// import userEvent from '@testing-library/user-event';
+import userEvent from "@testing-library/user-event";
 import ExampleDiv from "@/components/image/ExampleDiv";
 import { render, screen } from "@testing-library/react";
 import image from "public/images/my-picture.jpg";
+import { TextDecoder, TextEncoder } from "util";
+
+global.TextEncoder = TextEncoder;
+// @ts-ignore
+global.TextDecoder = TextDecoder;
+
+beforeAll(() => {
+  Object.defineProperty(window, "scroll", {
+    value: () => undefined,
+    writable: true,
+  });
+  global.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn(),
+  }));
+
+  Object.defineProperty(global.Image.prototype, "decode", {
+    get() {
+      return () => Promise.resolve();
+    },
+  });
+});
 
 describe("test ExampleDiv", () => {
-  test("When passing an image URL starting with https, it should be displayed.", () => {
+  test("When passing an image URL starting with https, it should be displayed.", async () => {
     const imageUrl =
       "https://github.blog/wp-content/uploads/2023/05/1200.630-Community-wLogo.png";
 
     render(<ExampleDiv imageUrl={imageUrl} />);
-    const img = screen.getByRole("img")
-    expect(img).toBeInTheDocument();
+    const img = screen.getByRole("img");
+    expect(img).toBeInTheDocument;
 
-    // userEvent.click(img);
-    // const minimizeButton = screen.getByRole("button");
-    // expect(minimizeButton).toBeInTheDocument();
+    userEvent.click(img);
+    const minimizeButton = await screen.findByRole("button");
+    expect(minimizeButton).toBeInTheDocument;
   });
 
   test("When passing a local image address, it should be displayed.", () => {
     const imagePath = "images/my-picture.jpg";
 
     render(<ExampleDiv imageUrl={imagePath} />);
-    expect(screen.getByRole("img")).toBeInTheDocument();
+    expect(screen.getByRole("img")).not.toBeInTheDocument;
   });
 
   test("When passing a local image mocked path, it should be displayed.", () => {
     const imagePath = image;
 
     render(<ExampleDiv imageUrl={imagePath} />);
-    expect(screen.getByRole("img")).toBeInTheDocument();
+    expect(screen.getByRole("img")).toBeInTheDocument;
   });
 });
