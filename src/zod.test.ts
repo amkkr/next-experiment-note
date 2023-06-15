fdba612bba1cec985f71460319f9a340dd5517a7
amkkr <55781271+amkkr@users.noreply.github.com>
2023-06-15 10:07:20 +0900

    add: zod test

---
+++ src/zod.test.ts
@@ -0,0 +1,27 @@
+import z from "zod";
+
+describe("test", () => {
+  test("zod", () => {
+    const testSchema = z.object({
+      id: z.number(),
+      name: z.string(),
+      descriptions: z
+        .object({
+          uuid: z.string(),
+          keshitai: z.string().optional(),
+        })
+        .omit({ keshitai: true }), // omitのためpassthroughを使えない
+    });
+
+    const result = testSchema.parse({
+      id: 1,
+      name: "namae",
+      descriptions: {
+        uuid: "uuid",
+        hoge: "hogehoge", // イメージとしてはopenapiにあってschemaにはないキ  ー
+      },
+    });
+
+    console.log({ result: result.descriptions });
+  });
+});
