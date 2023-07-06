4e44c6321751b46e640215758416f4ea90b8644c
amkkr <55781271+amkkr@users.noreply.github.com>
2023-07-07 00:51:57 +0900

    fix: mockを最小構成に

--- test/config/setup-jest.ts
+++ test/config/setup-jest.ts
@@ -5,15 +5,8 @@ global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
 
 beforeAll(() => {
-  Object.defineProperty(window, "scroll", {
-    value: () => undefined,
-    writable: true,
-  });
-
   global.ResizeObserver = jest.fn().mockImplementation(() => ({
     observe: jest.fn(),
-    unobserve: jest.fn(),
-    disconnect: jest.fn(),
   }));
 
   Object.defineProperty(global.Image.prototype, "decode", {
